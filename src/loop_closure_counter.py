adf2d149fae07934b76cdacfead844171e5a5bad
Riley Bridges <rlybrdgs@gmail.com>
2022-07-27 16:44:09 -0400

    changed loop closure counter to use official localization pose topic

---
+++ src/loop_closure_counter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import tf2_ros
+import rospy
+from std_msgs.msg import Int32
+from geometry_msgs.msg import Transform, Quaternion, Vector3, Pose
+
+
+def main():
+    sub = rospy.Subscriber("rtabmap/rtabmap/localization_pose", Pose, loop_closure_callback)
+    rospy.init_node("loop_closure_counter")
+
+    global loop_closure_count
+    loop_closure_count = 0
+
+    rospy.spin()
+
+def loop_closure_callback(msg):
+    global loop_closure_count
+    loop_closure_count += 1
+    rospy.loginfo(f"Loop Closure detected, count = {loop_closure_count}")
+
+if __name__ == "__main__":
+    main()
