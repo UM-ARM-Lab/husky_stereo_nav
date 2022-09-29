8cae487368e209c4d194578ea56db59b772d896e
Riley <alison.ryckman@gmail.com>
2022-09-29 14:41:22 -0400

    hue only + rgb to hsv conversion fix

--- src/kmeans.py
+++ src/kmeans.py
@@ -11,7 +11,7 @@ import sklearn.cluster
 import colorsys
 
 RGB_SCALE = 1/255;
-HSV_SCALE = 1/200;
+HSV_SCALE = 1/800;
 
 class KMeans:
     #cloud_pub: rospy.Publisher
@@ -28,8 +28,8 @@ class KMeans:
 
 def convertToHSV(rgb):
     hsv = np.array(rgb, copy=True)
-    for i in rgb[1]:
-        hsv[i] = colorsys.rgb_to_hsv(*rgb[i])
+    for i in rgb:
+        hsv[i] = colorsys.rgb_to_hsv(*i)
     return hsv
     
 def publish_hsv_filter(msg):
@@ -37,12 +37,23 @@ def publish_hsv_filter(msg):
         rgb = np.vstack(float_to_rgb(intensity) for intensity in points[:, 3])
         hsv = convertToHSV(rgb)
         hsv = HSV_SCALE * hsv
-        color_points = np.column_stack((points[:, -2], hsv[:, 0]))
-        print(f"color points: {color_points}\n")
+
+        # for looking at h only
+        color_points = hsv[:, 0]
+        color_points = color_points.reshape(-1, 1)
+
+        # zh
+        #color_points = np.column_stack((points[:, -2], hsv[:, 0]))
+        
         kmeans_color = sklearn.cluster.KMeans(n_clusters=5, random_state=0).fit(color_points)
         labels_color = kmeans_color.predict(color_points)
         outlier_ids_color = np.nonzero(labels_color == 1)[0]
         outliers_color = points[outlier_ids_color, :]
+
+        # list of hues of points in cluster 1
+        hue_list = hsv[outlier_ids_color]
+        # average hue in cluster 1
+        print(f"\n\nAVERAGE 0: {np.mean(hue_list, axis=0)[0] * 200}\n\n")
         color_cloud = pc2.create_cloud(msg.header, msg.fields, outliers_color)
         return color_cloud
 
